0456197d514cfd3cd2582ef3cc5e3f4af0e82859
Akshay Patil <144882215+Akshaycloud2004@users.noreply.github.com>
2026-03-30 08:23:38 +0000

    This is count file

--- sequence.tf
+++ sequence.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "vm" {
   count         = "3"
   ami           = "ami-0a7b62e89464a2d97"
   instance_type = "t3.micro"
   key_name      = "my.keypair"
   tags = {
-    Name = "server-${count.index}"
+    Name = "server"
   }
 }
